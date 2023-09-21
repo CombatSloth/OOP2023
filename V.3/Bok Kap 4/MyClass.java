2b156e06358d796dd386e6121b094e1bc46952e0
MrCombatsloth <144995813+CombatSloth@users.noreply.github.com>
2023-09-21 20:17:18 +0200

    Add files via upload

---
+++ V.3/Bok Kap 4/MyClass.java
@@ -0,0 +1,9 @@
+//A simple constructor
+
+public class MyClass {
+    int x;
+
+    MyClass() {    // This is the constructor for MyClass.      
+        x = 10;
+    }
+}
---
+++ V.3/Bok Kap 4/MyClass1.java
@@ -0,0 +1,8 @@
+// a parameterized constructor.
+public class MyClass1 {
+    int x;
+    
+    MyClass1(int i) {  // This constructor has a parameter. 
+        x = i;
+    }
+}
